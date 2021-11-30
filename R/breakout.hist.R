b2fd7e87e99a2db2c850185ad2cd302244fe74d4
colebeeaffinis <nicole.dede@selu.edu>
2021-11-30 19:04:18 +0000

    premerge

--- R/breakout.hist.R
+++ R/breakout.hist.R
@@ -1,4 +1,4 @@
-#'Plots a faceted histogram with multiple variable frequancies.
+#'Plots a faceted histogram with multiple variable frequencies.
 #'
 #'@peram dataset 
 #'@peram xcol A categorical column within the dataset to plot on the x axis
@@ -6,9 +6,12 @@
 #'@peram facetcol A categorical column to facet out the plots by
 #'@return plot A faceted out muli-variable histogram color coded with a legend 
 #'@usage
-#'breakout_hist()
+#'breakout.hist(Butterflies_dataset,Month,Species,Site)
 
-breakout_hist<-function(dataset,xcol,frqcol,facetcol){
+breakout.hist<-function(dataset,xcol,frqcol,facetcol){
   plot<-ggplot(data=dataset,aes(x=xcol,color=frqcol))+geom_histogram(stat=count)+facet_wrap(facets=vars(facetcol))
+  if(is.ggplot(plot))==FALSE
+  return("Error-not-ggplot")
+  }
   return(plot)
 }
